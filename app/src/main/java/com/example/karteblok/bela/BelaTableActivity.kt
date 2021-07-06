6ad8b96e736d19acc3809a8606fd3a9fe445eacf
tvrda34 <tvrtko.ivasic3@gmail.com>
2021-07-06 13:20:01 +0200

    Cards dealer info button function

--- app/src/main/java/com/example/karteblok/bela/BelaTableActivity.kt
+++ app/src/main/java/com/example/karteblok/bela/BelaTableActivity.kt
@@ -2,15 +2,18 @@ package com.example.karteblok.bela
 
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.graphics.ColorUtils
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.karteblok.R
 import com.example.karteblok.databinding.ActivityBelaTableBinding
 import com.example.karteblok.models.BelaModel
+import com.example.karteblok.models.dealersList
 
 class BelaTableActivity : AppCompatActivity() {
     private lateinit var binding: ActivityBelaTableBinding
+    private var tablePosition = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +33,12 @@ class BelaTableActivity : AppCompatActivity() {
             this.finish()
         }
 
+        //who's the dealer
+        binding.toolbar.dealerButton.setOnClickListener {
+            getDealer()
+            tablePosition = ++tablePosition % 4
+        }
+
         //mockana lista samo za prezentaciju
         val resultList = listOf(BelaModel(24, 138), BelaModel(162, 0), BelaModel(35, 127))
 
@@ -37,5 +46,10 @@ class BelaTableActivity : AppCompatActivity() {
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         binding.belaScoresRecycler.adapter = BelaScoreRecyclerAdapter(this, resultList)
         binding.belaScoresRecycler.setHasFixedSize(true)
+    }
+
+    private fun getDealer() {
+        val dealer = dealersList[tablePosition]
+        Toast.makeText(this, getString(dealer), Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/example/karteblok/models/BelaModel.kt
+++ app/src/main/java/com/example/karteblok/models/BelaModel.kt
@@ -1,6 +1,17 @@
 package com.example.karteblok.models
 
+import com.example.karteblok.R
+
 data class BelaModel(
     val score1: Int,
     val score2: Int
 )
+
+val dealersList = listOf(
+    R.string.team1_first_player,
+    R.string.team2_first_player,
+    R.string.team1_second_player,
+    R.string.team2_second_player
+)
+
+
